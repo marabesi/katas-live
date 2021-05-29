ab7c42aeafa9cc244505de22d1de757a601f77ae
Matheus Marabesi <matheus.marabesi@gmail.com>
2021-05-29 21:14:43 +0200

    test: failing first test case

--- test/MarsRoverTest.php
+++ test/MarsRoverTest.php
@@ -8,4 +8,11 @@ use PHPUnit\Framework\TestCase;
 class MarsRoverTest extends TestCase
 {
 
+    public function test_should_place_rover_facing_north() {
+        $rover = new MarsRover();
+
+        $coords = $rover->execute('M');
+
+        $this->assertEquals($coords, '0:1:N');
+    }
 }
