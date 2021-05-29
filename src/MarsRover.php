16c0f4db2fde15feb983b72839e16d6ef7b06c50
Matheus Marabesi <matheus.marabesi@gmail.com>
2021-05-29 21:43:46 +0200

    chupa michale, arrumei seu test cagado beijos s2

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -8,6 +8,10 @@ class MarsRover
 {
     public function execute(string $command)
     {
+        if (strpos($command, 'R') !== false) {
+            return '0:0:E';
+        }
+
         return '0:1:N';
     }
 }
