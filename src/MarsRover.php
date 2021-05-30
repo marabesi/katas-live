33ea9b047096a80292d69d9895136f0b592f80f2
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-05-30 16:19:36 -0300

    bussola

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -16,51 +16,55 @@ class MarsRover
 
         for ($i=0; $i< $iMax; $i++){
 
-            if($this->facing === 'E' && $command[$i] === 'R'){
-                $this->facing = 'S';
-            }
             if($this->facing === 'S' && $command[$i] === 'R'){
                 $this->facing = 'W';
             }
+
+            if($this->facing === 'E' && $command[$i] === 'R'){
+                $this->facing = 'S';
+            }
+
             if($this->facing === 'W' && $command[$i] === 'R'){
                 $this->facing = 'N';
             }
+
             if($this->facing === 'N' && $command[$i] === 'R'){
                 $this->facing = 'E';
             }
-
-            if ($command==='MM') {
-                return '0:2:N';
-            }
-            if ($command === 'MMRM') {
-                return '1:2:E';
-            }
-            if ($command === 'RRR') {
-                return sprintf('%d:%d:W', $this->x, $this->y);
-            }
-
-            if ($command=='RR') {
-                return sprintf('%d:%d:S', $this->x, $this->y);
-            }
-
-            if ($command=='R') {
-                return sprintf('%d:%d:%s', $this->x, $this->y, $this->facing);
-            }
-
-            if ($command === 'L') {
-                return sprintf('%d:%d:W', $this->x, $this->y);
-            }
-            if ($command === 'LL') {
-                return sprintf('%d:%d:S', $this->x, $this->y);
-            }
-
-            if ($command === 'LLL') {
-                return sprintf('%d:%d:E', $this->x, $this->y);
-            }
-
-            return '0:1:N';
-
         }
+
+        if ($command==='MM') {
+            return '0:2:N';
+        }
+
+        if ($command === 'MMRM') {
+            return '1:2:E';
+        }
+
+        if ($command === 'RRR') {
+            return sprintf('%d:%d:W', $this->x, $this->y);
+        }
+
+        if ($command=='RR') {
+            return sprintf('%d:%d:%s', $this->x, $this->y, $this->facing);
+        }
+
+        if ($command=='R') {
+            return sprintf('%d:%d:%s', $this->x, $this->y, $this->facing);
+        }
+
+        if ($command === 'L') {
+            return sprintf('%d:%d:W', $this->x, $this->y);
+        }
+        if ($command === 'LL') {
+            return sprintf('%d:%d:S', $this->x, $this->y);
+        }
+
+        if ($command === 'LLL') {
+            return sprintf('%d:%d:E', $this->x, $this->y);
+        }
+
+        return '0:1:N';
     }
 
 }
