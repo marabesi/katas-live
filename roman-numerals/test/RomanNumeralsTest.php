a257d29c3cc84c484226bae643c2413881d5a46e
Matheus Marabesi <matheus.marabesi@gmail.com>
2021-06-19 22:07:58 +0200

    refactor: tested em orderm numérica

--- roman-numerals/test/RomanNumeralsTest.php
+++ roman-numerals/test/RomanNumeralsTest.php
@@ -12,18 +12,18 @@ class RomanNumeralsTest extends TestCase
     public function data(): array {
         return [
             [1, 'I'],
-            [5, 'V'],
-            [10, 'X'],
-            [100, 'C'],
-            [500, 'D'],
-            [1000, 'M'],
             [2, 'II'],
             [3, 'III'],
             [4, 'IV'],
+            [5, 'V'],
             [6, 'VI'],
             [7, 'VII'],
             [8, 'VIII'],
             [9, 'VIIII'],
+            [10, 'X'],
+            [100, 'C'],
+            [500, 'D'],
+            [1000, 'M'],
         ];
     }
 
