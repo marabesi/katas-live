c87d1d687e30412a0cde168dcc208be43056d9e3
Matheus Marabesi <matheus.marabesi@gmail.com>
2021-06-19 20:56:11 +0200

    refactor: data provider

--- roman-numerals/test/RomanNumeralsTest.php
+++ roman-numerals/test/RomanNumeralsTest.php
@@ -9,23 +9,20 @@ use PHPUnit\Framework\TestCase;
 class RomanNumeralsTest extends TestCase
 {
 
-    public function test_should_convert_number_1_into_I() {
+    public function data(): array {
+        return [
+            [1, 'I'],
+            [5, 'V'],
+            [10, 'X'],
+        ];
+    }
+
+    /**
+     * @dataProvider data
+     */
+    public function test_should_convert_decimal_to_roman_numeral(int $amount, string $romanNumber) {
         $romanNumberals = new RomanNumerals();
-        $romanNumber = $romanNumberals->convert(1);
-        $this->assertEquals('I', $romanNumber);
-    }
-
-    public function test_should_convert_number_5_into_V()
-    {
-        $romanNumberls = new RomanNumerals();
-        $romanNumber = $romanNumberls->convert(5);
-        $this->assertEquals('V', $romanNumber);
-    }
-
-    public function test_should_convert_number_10_into_X()
-    {
-        $romanNumberls = new RomanNumerals();
-        $romanNumber = $romanNumberls->convert(10);
-        $this->assertEquals('X', $romanNumber);
+        $convertion = $romanNumberals->convert($amount);
+        $this->assertEquals($romanNumber, $convertion);
     }
 }
