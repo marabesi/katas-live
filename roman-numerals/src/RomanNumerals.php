156c6d666bde6d374fdc7b67605044c4a44f61bf
Aron Peyroteo <aron@mqx.com.br>
2021-06-26 17:18:15 -0300

    refactor: removendo testes  sem uso

--- roman-numerals/src/RomanNumerals.php
+++ roman-numerals/src/RomanNumerals.php
@@ -5,7 +5,8 @@ namespace Kata;
 class RomanNumerals
 {
 
-    public function convert(int $amount): string {
+    public function convert(int $amount): string
+    {
 
         if ($amount < 1000) {
             $hundreds = new HundredsNumberConverter($amount);
@@ -15,7 +16,7 @@ class RomanNumerals
                 $dozens = new DozensNumberConverter($hundreds->divisionRest());
                 $expectedResult .= $dozens->toRoman();
 
-                if ($dozens->divisionRest() > 0){
+                if ($dozens->divisionRest() > 0) {
                     $number = new UnitNumberConverter($dozens->divisionRest());
                     $expectedResult .= $number->toRoman();
                 }
@@ -23,52 +24,8 @@ class RomanNumerals
             return $expectedResult;
         }
 
-        if ($amount === 294) {
-            $romanNumber =  $this->unitConverter($amount,'C','D','M',100);
-            return $romanNumber.'XCIV';
-        }
-
-        if ($amount >= 100 && $amount <= 900) {
-            return $this->unitConverter($amount,'C','D','M',100);
-        }
-        if ($amount === 500) {
-            return 'D';
-        }
         if ($amount === 1000) {
             return 'M';
         }
-    }
-
-    private function unitConverter(
-        int $amount,
-        string $symbol,
-        string $midSymbol,
-        string $postSymbol,
-        int $divider = 1
-    ) : string {
-        $amount = $amount / $divider;
-        if ($amount >= 1 && $amount <= 3) {
-            return $this->repeatStringForm($amount, 1, $symbol);
-        }
-        if ($amount === 4) {
-            return sprintf('%s%s', $symbol, $midSymbol);
-        }
-        if ($amount === 9) {
-            return sprintf('%s%s', $symbol, $postSymbol);
-        }
-
-        $i = '';
-
-        while($amount >= 6 && $amount <= 8) {
-          $i .= $symbol;
-          $amount --;
-        }
-
-        return sprintf('%s%s', $midSymbol, $i);
-    }
-
-    private function repeatStringForm(int $amount, int $module, string $stringToRepeat): string {
-        $rest = floor($amount / $module);
-        return str_repeat($stringToRepeat, $rest);
     }
 }
