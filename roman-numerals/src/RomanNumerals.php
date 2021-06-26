70052488bc0f9440f912f758b00b0c47f6ad96d5
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-06-26 17:27:38 -0300

    test: teste de 1000 passando

--- roman-numerals/src/RomanNumerals.php
+++ roman-numerals/src/RomanNumerals.php
@@ -7,25 +7,24 @@ class RomanNumerals
 
     public function convert(int $amount): string
     {
+        if ($amount < 4000) {
+            $thousand = new ThousandNumberConverter($amount);
+            $expectedResult = $thousand->toRoman();
+            if ($thousand->divisionRest() < 1000) {
+                $hundreds = new HundredsNumberConverter($thousand->divisionRest());
+                $expectedResult .= $hundreds->toRoman();
 
-        if ($amount < 1000) {
-            $hundreds = new HundredsNumberConverter($amount);
-            $expectedResult = $hundreds->toRoman();
+                if ($hundreds->divisionRest() < 100) {
+                    $dozens = new DozensNumberConverter($hundreds->divisionRest());
+                    $expectedResult .= $dozens->toRoman();
 
-            if ($hundreds->divisionRest() < 100) {
-                $dozens = new DozensNumberConverter($hundreds->divisionRest());
-                $expectedResult .= $dozens->toRoman();
-
-                if ($dozens->divisionRest() > 0) {
-                    $number = new UnitNumberConverter($dozens->divisionRest());
-                    $expectedResult .= $number->toRoman();
+                    if ($dozens->divisionRest() > 0) {
+                        $number = new UnitNumberConverter($dozens->divisionRest());
+                        $expectedResult .= $number->toRoman();
+                    }
                 }
             }
             return $expectedResult;
-        }
-
-        if ($amount === 1000) {
-            return 'M';
         }
     }
 }
