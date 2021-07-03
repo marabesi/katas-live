90bf063f6242d4b8f6937c5595d19e6bf22957e4
Aron Peyroteo <aron@mqx.com.br>
2021-07-03 17:46:08 -0300

    refactor: extrair moveRight para nova class

---
+++ mars-rover/typescript/src/MoveRight.ts
@@ -0,0 +1,23 @@
+import MarsRover from "./MarsRover";
+import {Compass} from "./Compass";
+
+export class MoveRight {
+    private rover: MarsRover;
+    constructor(rover: MarsRover) {
+        this.rover = rover
+    }
+    move() {
+        if (this.rover.facing === Compass.EAST) {
+          this.rover.facing = Compass.SOUTH
+          return
+        }
+        if (this.rover.facing === Compass.NORTH) {
+          this.rover.facing = Compass.EAST
+          return
+        }
+        if (this.rover.facing === Compass.SOUTH) {
+          this.rover.facing = Compass.WEST
+          return
+        }
+    }
+}
