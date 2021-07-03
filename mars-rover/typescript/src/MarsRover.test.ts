4d1e8d5a6f021de929434bbdf5c4c3dd13ff5634
Matheus Marabesi <matheus.marabesi@gmail.com>
2021-07-03 21:34:56 +0200

    test: falhando mover para direita

--- mars-rover/typescript/src/MarsRover.test.ts
+++ mars-rover/typescript/src/MarsRover.test.ts
@@ -8,6 +8,7 @@ describe('Mars rover', () => {
       [{ command: 'L', expectedPosition: '0:0:W'}],
       [{ command: 'R', expectedPosition: '0:0:E'}],
       [{ command: 'LL', expectedPosition: '0:0:S'}],
+      [{ command: 'RR', expectedPosition: '0:0:S'}],
     ]
   )('move mars rover based on %s', ({ command, expectedPosition }) => {
     const rover = new MarsRover(new Grid(10, 10))
