e18781847009b0867e83e05c5eb0e182931adeb9
Aron Peyroteo <aron@mqx.com.br>
2021-07-03 16:42:19 -0300

    test: falhando movendo para direita e andando

--- mars-rover/typescript/src/MarsRover.test.ts
+++ mars-rover/typescript/src/MarsRover.test.ts
@@ -10,6 +10,7 @@ describe('Mars rover', () => {
       [{ command: 'LL', expectedPosition: '0:0:S'}],
       [{ command: 'RR', expectedPosition: '0:0:S'}],
       [{ command: 'ML', expectedPosition: '0:1:W'}],
+      [{ command: 'RM', expectedPosition: '1:0:E'}],
     ]
   )('move mars rover based on %s', ({ command, expectedPosition }) => {
     const rover = new MarsRover(new Grid(10, 10))
