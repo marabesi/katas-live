42d2a7ac7ec0a81016872f1683c8ce85b30d875f
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-07-24 18:10:31 -0300

    acceptance: encantamento de agilidade

--- enchantment/test/DuranceAcceptTest.php
+++ enchantment/test/DuranceAcceptTest.php
@@ -12,14 +12,19 @@ class DuranceAcceptTest extends TestCase
 
     public function test_can_enchant_fire_weapon()
     {
-        $this->markTestIncomplete('aceitação');
+        $this->markTestSkipped('aceitacao');
         $describeWeapon = <<<EOT
 Inferno Dagger of the Nooblet
 5 - 10 attack damage
 1.2 attack speed
 +5 fire damage
 EOT;
-        $durance = new Durance();
+        $weapon = new Weapon('Dagger of the Nooblet', [
+            'attack damage' => '5 - 10',
+            'attack speed' => 1.2
+        ]);
+        $durance = new Durance($weapon);
+        $durance->enchant();
         $this->assertEquals($describeWeapon,$durance->describeWeapon());
     }
 
