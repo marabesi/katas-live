265949090354d59f83c8be66da9fa028e4afe4f9
Aron Peyroteo <aron@mqx.com.br>
2021-08-28 16:38:31 -0300

    regex para separador passando

--- string-calculator/php/src/StringCalculator.php
+++ string-calculator/php/src/StringCalculator.php
@@ -9,9 +9,9 @@ class StringCalculator
     public function add(string $numbers) : int
     {
         $separator = ',';
-        if(preg_match("^//(\w){1}\n", $numbers, $matches)){
-            $separator = ';';
-            $numbers = str_replace("//;\n", '', $numbers);
+        if(preg_match('/^\/\/(.*)\\n(.*)/', $numbers, $matches)){
+            $separator = $matches[1];
+            $numbers = $matches[2];
         }
         $numbers = str_replace(["\n"], $separator, $numbers);
         $numbers = explode($separator, $numbers);
