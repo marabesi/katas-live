f86e4507a37b343a984761a6e2272db97968c9ca
marabesi <matheus.marabesi@gmail.com>
2021-08-28 20:46:56 +0200

    string calculator string vazia retorna zero passando

--- string-calculator/php/test/StringCalculatorTest.php
+++ string-calculator/php/test/StringCalculatorTest.php
@@ -8,4 +8,9 @@ use PHPUnit\Framework\TestCase;
 
 class StringCalculatorTest extends TestCase
 {
+    public function test_empty_string_returns_zero() {
+        $calculator = new StringCalculator();
+        $result = $calculator->add("");
+        $this->assertEquals(0, $result);
+    }
 }
