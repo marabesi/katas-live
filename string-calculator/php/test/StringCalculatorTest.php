a05e0dfebe3157086d6a580b144d37bdcd656f13
marabesi <matheus.marabesi@gmail.com>
2021-08-28 21:05:09 +0200

    refactor: código do teste com data provider

--- string-calculator/php/test/StringCalculatorTest.php
+++ string-calculator/php/test/StringCalculatorTest.php
@@ -8,26 +8,22 @@ use PHPUnit\Framework\TestCase;
 
 class StringCalculatorTest extends TestCase
 {
-    public function test_empty_string_returns_zero() {
-        $calculator = new StringCalculator();
-        $result = $calculator->add("");
-        $this->assertEquals(0, $result);
+    public function stringsWithComma(): array
+    {
+        return [
+            ["", 0],
+            ["4", 4],
+            ["1,2", 3],
+            ["1,2,3,4,5,6,7,8,9", 45],
+        ];
     }
 
-    public function test_string_with_number_four_returns_four() {
+    /**
+     * @dataProvider stringsWithComma
+     */
+    public function test_should_sum_strings_with_comma($strings, $expected) {
         $calculator = new StringCalculator();
-        $result = $calculator->add("4");
-        $this->assertEquals(4, $result);
-    }
-
-    public function test_string_sum_number_one_and_two() {
-        $calculator = new StringCalculator();
-        $result = $calculator->add("1,2");
-        $this->assertEquals(3, $result);
-    }
-    public function test_string_sum_arbitray_number() {
-        $calculator = new StringCalculator();
-        $result = $calculator->add("1,2,3,4,5,6,7,8,9");
-        $this->assertEquals(45, $result);
+        $result = $calculator->add($strings);
+        $this->assertEquals($expected, $result);
     }
 }
