aa79822016c84004d51b476a3f152bf01750d222
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-08-28 16:11:57 -0300

    passando: teste somando numero com quebra de linhas

--- string-calculator/php/src/StringCalculator.php
+++ string-calculator/php/src/StringCalculator.php
@@ -8,6 +8,7 @@ class StringCalculator
 {
     public function add(string $numbers) : int
     {
+        $numbers = str_replace(["\n"],',', $numbers);
         $numbers = explode(',', $numbers);
         return (int) array_sum($numbers);
     }
