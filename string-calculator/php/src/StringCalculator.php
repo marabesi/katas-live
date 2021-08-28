6d658dc76b9d6c998927e368c681c722cdd50786
marabesi <matheus.marabesi@gmail.com>
2021-08-28 22:18:01 +0200

    refactor: code style

--- string-calculator/php/src/StringCalculator.php
+++ string-calculator/php/src/StringCalculator.php
@@ -11,21 +11,26 @@ class StringCalculator
      */
     public function add(string $numbers) : int
     {
-        $separator = ',';
-        if (preg_match('/^\/\/(.*)\\n(.*)/', $numbers, $matches)) {
-            [, $separator,$numbers] = $matches;
-        }
+        $parsedNumbers = $this->parse($numbers);
 
-        $numbers = str_replace(["\n"], $separator, $numbers);
-
-        $numbers = explode($separator, $numbers);
-
-        $negatives = array_filter($numbers, fn($number) => (int) $number < 0);
+        $negatives = array_filter($parsedNumbers, fn($number) => (int) $number < 0);
 
         if (count($negatives)) {
             throw new NegativeAreNotAllowed();
         }
 
-        return (int) array_sum($numbers);
+        return (int) array_sum($parsedNumbers);
+    }
+
+    public function parse(string $numbers): array
+    {
+        $separator = ',';
+        if (preg_match('/^\/\/(.*)\\n(.*)/', $numbers, $matches)) {
+            [, $separator, $numbers] = $matches;
+        }
+
+        $numbers = str_replace(["\n"], $separator, $numbers);
+
+        return explode($separator, $numbers);
     }
 }
