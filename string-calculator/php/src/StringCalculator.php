7d8eff340c21b8cc3bcf37e0e14bdb9956ef7b42
Aron Peyroteo <aron@mqx.com.br>
2021-08-28 15:58:01 -0300

    refactor: numeros arbitrarios passando

--- string-calculator/php/src/StringCalculator.php
+++ string-calculator/php/src/StringCalculator.php
@@ -8,12 +8,7 @@ class StringCalculator
 {
     public function add(string $numbers) : int
     {
-        if ($numbers == ''){
-            return 0;
-        }
-        if($numbers == '1,2'){
-            return 3;
-        }
-        return (int) $numbers;
+        $numbers = explode(',', $numbers);
+        return (int) array_sum($numbers);
     }
 }
