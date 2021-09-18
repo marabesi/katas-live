677b347b7536c849400dadeb341b3b2c8d63d90a
marabesi <matheus.marabesi@gmail.com>
2021-09-18 20:57:01 +0200

    refactorL extracted method divisor

--- leap-year/php/src/LeapYear.php
+++ leap-year/php/src/LeapYear.php
@@ -7,10 +7,15 @@ class LeapYear
 {
     public function isLeapYear(int $year): bool
     {
-        if ($year % 4 === 0) {
+        if ($this->isDivisibleBy($year, 4)) {
             return true;
         }
 
         return false;
+    }
+
+    private function isDivisibleBy(int $year, int $divisor): bool
+    {
+        return $year % $divisor === 0;
     }
 }
