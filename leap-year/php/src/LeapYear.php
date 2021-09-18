9821f1bb4f051f8f74ad024ff3038875e933056a
marabesi <matheus.marabesi@gmail.com>
2021-09-18 21:20:48 +0200

    refactor: extracted method isNotDivisiable

--- leap-year/php/src/LeapYear.php
+++ leap-year/php/src/LeapYear.php
@@ -8,7 +8,7 @@ class LeapYear
     public function isLeapYear(int $year): bool
     {
         if ($this->isDivisibleBy($year, 4) || $this->isDivisibleBy($year, 400)) {
-            if ($this->isDivisibleBy($year, 100) && !$this->isDivisibleBy($year, 400)) {
+            if ($this->isDivisibleBy($year, 100) && $this->isNotDivisibleBy($year, 400)) {
                 return false;
             }
             return true;
@@ -19,5 +19,10 @@ class LeapYear
     private function isDivisibleBy(int $year, int $divisor): bool
     {
         return $year % $divisor === 0;
+    }
+
+    private function isNotDivisibleBy(int $year, int $divisor): bool
+    {
+        return $year % $divisor > 0;
     }
 }
--- leap-year/php/test/LeapYearTest.php
+++ leap-year/php/test/LeapYearTest.php
@@ -24,10 +24,8 @@ class LeapYearTest extends TestCase
             [1996, true],
             [1600, true],
             [1800, false],
-
             [1600, true],
             [2000, true],
-
             [1700, false],
             [2100, false],
             [1800, false],
