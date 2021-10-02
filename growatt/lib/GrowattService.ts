e7446c5f1cf06d5bf47fa6ec403452f0eba43b13
marabesi <matheus.marabesi@gmail.com>
2021-10-02 22:54:16 +0200

    refactor: move plantId to property class

--- growatt/lib/GrowattService.ts
+++ growatt/lib/GrowattService.ts
@@ -1,20 +1,22 @@
-import {ApiInterface} from "./types";
+import {ApiInterface} from './types';
 
 export class GrowattService {
     constructor(private growattApi: ApiInterface) { }
 
+    private readonly plantId = '582073'
+
     async coalSummary() {
         const response = await this.growattApi.getData();
-        return parseFloat(response['582073'].plantData.coal);
+        return parseFloat(response[this.plantId].plantData.coal);
     }
 
     async co2Summary(): Promise<Number> {
         const response = await this.growattApi.getData();
-        return parseFloat(response['582073'].plantData.co2);
+        return parseFloat(response[this.plantId].plantData.co2);
     }
 
     async energySummary(): Promise<Number> {
         const response = await this.growattApi.getData();
-        return parseFloat(response['582073'].plantData.eTotal)
+        return parseFloat(response[this.plantId].plantData.eTotal)
     }
 }
