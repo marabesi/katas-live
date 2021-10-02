65f1e711974ac689bf7f6eacce4a895b818f969f
Aron Peyroteo <aron@mqx.com.br>
2021-10-02 17:35:17 -0300

    refactor: GrowattService

--- growatt/lib/GrowattService.spec.ts
+++ growatt/lib/GrowattService.spec.ts
@@ -1,28 +1,31 @@
 import {error, ErrorApiStub, SuccessApiStub} from './api/ApiStub'
 import {GrowattService} from "./GrowattService";
 
-describe('fetch co2', () => {
-    test('should return co2 information from the api', async () => {
-        const co2 = new GrowattService(new SuccessApiStub())
-        const burnedCo2 = await co2.co2Summary()
+describe('fetch Growatt apiData', () => {
+    let growattServiceSuccess: GrowattService
+    let growattServiceError: GrowattService
 
+    beforeEach(() => {
+        growattServiceSuccess = new GrowattService(new SuccessApiStub())
+        growattServiceError = new GrowattService(new ErrorApiStub())
+    })
+
+    test('should return co2 information from the api', async () => {
+        const burnedCo2 = await growattServiceSuccess.co2Summary()
         expect(burnedCo2).toEqual(1246.9)
     })
 
     test('should display error if request fails', async () => {
-        const co2 = new GrowattService(new ErrorApiStub())
-        await expect(co2.co2Summary()).rejects.toEqual(error)
+        await expect(growattServiceError.co2Summary()).rejects.toEqual(error)
     })
 
     test('should fetch coal from growatt', async() => {
-        const coal = new GrowattService(new SuccessApiStub());
-        const coalSummary = await coal.coalSummary();
+        const coalSummary = await growattServiceSuccess.coalSummary();
         expect(coalSummary).toEqual(666);
     });
 
     test('should fetch total energy', async () => {
-        const energy = new GrowattService(new SuccessApiStub());
-        const energySummary = await energy.energySummary();
+        const energySummary = await growattServiceSuccess.energySummary();
         expect(energySummary).toEqual(6666);
     })
 })
