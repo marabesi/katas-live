8222b9145af73567ff2c7f9d955737cf46070e85
marabesi <matheus.marabesi@gmail.com>
2021-10-02 20:51:16 +0200

    refactor(growatt): renamed describe

--- growatt/lib/Co2Acceptance.spec.ts
+++ growatt/lib/Co2Acceptance.spec.ts
@@ -2,7 +2,7 @@ import { Co2Summary } from './Co2Summary'
 import { GrowattApi } from './GrowattApi'
 import any = jasmine.any
 
-describe('Fetch quantity of Co2 not burned', () => {
+describe('Fetch (from real api) the quantity of Co2 not burned', () => {
  test('should give the co2 not used as a kg number', async () => {
   const co2Summary = new Co2Summary(new GrowattApi());
   expect(await co2Summary.fetch()).toEqual(any(Number))
