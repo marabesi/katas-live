a6868e5aa22e474ac071750e3a3f79a17a94c68b
marabesi <matheus.marabesi@gmail.com>
2021-10-02 23:02:21 +0200

    refactor: renamed growattAcceptance service

--- growatt/lib/GrowattAcceptance.spec.ts
+++ growatt/lib/GrowattAcceptance.spec.ts
@@ -3,18 +3,17 @@ import {GrowattService} from "./GrowattService";
 import any = jasmine.any;
 
 describe('Fetch (from real api) the quantity of Co2 not burned', () => {
+ const growattService = new GrowattService(new GrowattApi());
+
  test('should give the co2 not used as a kg number', async () => {
-  const co2Summary = new GrowattService(new GrowattApi());
-  expect(await co2Summary.co2Summary()).toEqual(any(Number))
+  expect(await growattService.co2Summary()).toEqual(any(Number))
  });
 
  test('should give the coal not burned as a kg number', async () => {
-  const coalSummary = new GrowattService(new GrowattApi());
-  expect(await coalSummary.coalSummary()).toEqual(any(Number))
+  expect(await growattService.coalSummary()).toEqual(any(Number))
  });
 
  test('should give the total amount of energy generated', async () => {
-  const coalSummary = new GrowattService(new GrowattApi());
-  expect(await coalSummary.energySummary()).toEqual(any(Number))
+  expect(await growattService.energySummary()).toEqual(any(Number))
  });
 });
