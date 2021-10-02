4942547ce8b4a213ef8f896efc4c80999e94d8e1
Michael de Oliveira Ferreira <oliveiramichael@outlook.com>
2021-10-02 17:10:02 -0300

    refactoring: criacao growatt service

---
+++ growatt/lib/GrowattService.spec.ts
@@ -0,0 +1,22 @@
+import {error, ErrorApiStub, SuccessApiStub} from './ApiStub'
+import {GrowattService} from "./GrowattService";
+
+describe('fetch co2', () => {
+    test('should return co2 information from the api', async () => {
+        const co2 = new GrowattService(new SuccessApiStub())
+        const burnedCo2 = await co2.co2Summary()
+
+        expect(burnedCo2).toEqual(1246.9)
+    })
+
+    test('should display error if request fails', async () => {
+        const co2 = new GrowattService(new ErrorApiStub())
+        await expect(co2.co2Summary()).rejects.toEqual(error)
+    })
+
+    test('should fetch coal from growatt', async() => {
+        const coal = new GrowattService(new SuccessApiStub());
+        const coalSummary = await coal.coalSummary();
+        expect(coalSummary).toEqual(666);
+    });
+})
