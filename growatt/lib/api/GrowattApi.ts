2c43a170e72bd73168fecc42822b10dfa26d6d7d
Aron Peyroteo <aron@mqx.com.br>
2021-10-09 17:38:59 -0300

    remove comments

--- growatt/lib/api/GrowattApi.ts
+++ growatt/lib/api/GrowattApi.ts
@@ -4,13 +4,6 @@ import { ApiInterface, ApiResponse } from '../types'
 
 const growatt = require('growatt')
 
-// const growatt = new api({})
-// let login = await growatt.login(user,passwort).catch(e => {console.log(e)})
-// console.log('login:',login)
-// let getAllPlantData = await growatt.getAllPlantData(options).catch(e => {console.log(e)})
-// console.log('getAllPlatData:',JSON.stringify(getAllPlantData,null,' '));
-// let logout = await growatt.logout().catch(e => {console.log(e)})
-// console.log('logout:',logout)
 
 const user = process.env.GROWATT_USER || 'root'
 const password = process.env.GROWATT_PASS || 'charmander'
