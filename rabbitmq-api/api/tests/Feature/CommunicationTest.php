4b676c05e210edf71179c7580db1b0f1b6e155de
marabesi <matheus.marabesi@gmail.com>
2021-11-06 21:40:15 +0100

    failing publish queue message

--- rabbitmq-api/api/tests/Feature/CommunicationTest.php
+++ rabbitmq-api/api/tests/Feature/CommunicationTest.php
@@ -2,12 +2,21 @@
 
 namespace App\Tests;
 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 use PHPUnit\Framework\TestCase;
 
 class CommunicationTest extends TestCase
 {
 
-    public function test_example() {
+    public function test_can_publish_a_message() {
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+        $channel->queue_declare('hello', false, false, false, false);
+
+        $msg = new AMQPMessage('Hello World!');
+        $channel->basic_publish($msg, '', 'hello');
+        
         $this->assertTrue(true);
     }
 }
