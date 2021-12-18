4009e54c81bd1966449a8b173de389efc5ee7bcb
marabesi <matheus.marabesi@gmail.com>
2021-12-18 20:16:03 +0100

    chore: reset smart fridge

--- smart-fridge/src/SmartFridge.php
+++ smart-fridge/src/SmartFridge.php
@@ -2,21 +2,6 @@
 
 namespace Kata;
 
-use DateTime;
-
 class SmartFridge
 {
-    private array $items = [];
-
-    public function addItem(Item $item)
-    {
-        $item->createTimestamp();
-        $this->items[] = $item;
-        return $this->items;
-    }
-
-    public function getItems(): array
-    {
-        return $this->items;
-    }
 }
--- smart-fridge/test/SmartFridgeTest.php
+++ smart-fridge/test/SmartFridgeTest.php
@@ -3,62 +3,8 @@ declare(strict_types=1);
 
 namespace Kata\Test;
 
-use DateInterval;
-use Kata\Item;
-use Kata\SmartFridge;
 use PHPUnit\Framework\TestCase;
-use DateTime;
 
 class SmartFridgeTest extends TestCase
 {
-
-    public function test_fridge_starts_empty()
-    {
-        $fridge = new SmartFridge();
-        $this->assertCount(0, $fridge->getItems());
-    }
-    
-    public function test_should_add_item_into_fridge()
-    {
-        $fridge = new SmartFridge();
-        $fridge->addItem(new Item('Feijoada', new DateTime()));
-        $this->assertCount(1, $fridge->getItems());
-    }
-
-    public function test_should_add_2_items_into_fridge()
-    {
-        $fridge = new SmartFridge();
-        $fridge->addItem(new Item('Feijoada', new DateTime()));
-        $fridge->addItem(new Item('Coxinha', new DateTime()));
-
-        $this->assertCount(2, $fridge->getItems());
-    }
-
-    public function test_should_add_items_with_expiration_date_for_tomorrow()
-    {
-        $tomorrow = new DateTime();
-        $tomorrow->add(new DateInterval('P1D'));
-
-        $fridge = new SmartFridge();
-        $fridge->addItem(new Item('Feijoada', $tomorrow));
-
-        $items = $fridge->getItems();
-        $item = $items[0];
-
-        $this->assertEquals($item->expiration(), $tomorrow);
-    }
-
-    public function test_should_add_items_with_now_as_added_timestamp()
-    {
-        $tomorrow = new DateTime();
-        $tomorrow->add(new DateInterval('P1D'));
-
-        $fridge = new SmartFridge();
-        $fridge->addItem(new Item('Feijoada', $tomorrow));
-
-        $items = $fridge->getItems();
-        $item = $items[0];
-
-        $this->assertEquals($item->timestamp(), new DateTime());
-    }
 }
